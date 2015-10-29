bb0f40ce05944f7a60699a9d749925a63954145d
Junpei <kusumoto.0513.jp@gmail.com>
2015-10-29 23:09:01 +0900

    課題提出

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,20 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+
+  def initialize(purchased_ever)
+    @EXCHANGEABLE = 3.freeze
+    @purchased_sum = purchased_ever
+  end
+
+  def total_count_for(amount)
+    bins = total_bins(amount)
+    amount + omake_quantity(bins)
+  end
+
+  def total_bins(amount)
+    @purchased_sum += amount
+  end
+
+  def omake_quantity(bins)
+    bins / @EXCHANGEABLE
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+
+  context 'when 0 bin' do
+    let(:bonus_drink) { BonusDrink.new(0) }
+    specify { expect(bonus_drink.total_count_for(0)).to eq 0 }
+  end
+  context 'when 1 bin' do
+    let(:bonus_drink) { BonusDrink.new(0) }
+    specify { expect(bonus_drink.total_count_for(1)).to eq 1 }
+  end
+  context 'when 3 bins' do
+    let(:bonus_drink) { BonusDrink.new(1) }
+    specify { expect(bonus_drink.total_count_for(3)).to eq 4 }
+  end
+  context 'when 11 bins' do
+    let(:bonus_drink) { BonusDrink.new(4) }
+    specify { expect(bonus_drink.total_count_for(11)).to eq 16 }
+  end
+  context 'when bought 100 bins' do
+    let(:bonus_drink) { BonusDrink.new(0) }
+    specify { expect(bonus_drink.total_count_for(100)).to eq 133 }
+  end
 end
