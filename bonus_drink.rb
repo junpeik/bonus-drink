643c4b95aa2051151409662fbc2066a856d116ac
Junpei <kusumoto.0513.jp@gmail.com>
2015-11-06 00:47:19 +0900

    ロジック訂正

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,20 +1,11 @@
 class BonusDrink
-
-  def initialize(purchased_ever)
-    @EXCHANGEABLE = 3.freeze
-    @purchased_sum = purchased_ever
-  end
-
-  def total_count_for(amount)
-    bins = total_bins(amount)
-    amount + omake_quantity(bins)
-  end
-
-  def total_bins(amount)
-    @purchased_sum += amount
-  end
-
-  def omake_quantity(bins)
-    bins / @EXCHANGEABLE
+  def self.total_count_for(amount)
+    init = amount
+    exchangeable = 0
+    until amount < 3 do
+      amount = amount - 3 + 1
+      exchangeable += 1
+    end
+    init + exchangeable
   end
 end
